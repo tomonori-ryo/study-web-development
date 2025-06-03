acf97eecaebfd00baba66b4fc19431c9f0c88d2f
tomo <tomonoriryou4@icloud.com>
2025-06-03 15:12:22 +0900

    add  files

--- sample3.php
+++ sample3.php
@@ -1,15 +1,1 @@
-<?php
-//商品ごとの価格
-$apples = 100;
-$meat = 1000;
-$eggs = 200;
-// 税抜き合計
-$total = $apples + $meat + $eggs;
-//税率１０％バージョン
-$taxRate = 0.10;
-//税込合計
-$totalWithTax = $total * (1+$taxRate);
-//表示
-echo "税抜き合計金額：{$total}円<br>";
-echo "税込み合計金額：" . round($totalWithTax) . "円<br>";
-?>
+<?php echo 'This is sample3.php on practice branch.'; ?>
